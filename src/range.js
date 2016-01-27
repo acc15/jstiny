88e1182122fc48c518246b0bf33f67d813d5c6f4
Vyacheslav Mayorov <Vyacheslav.Mayorov@smartgames-holdings.ru>
2016-01-27 12:32:52 +0300

    range

--- src/range.js
+++ src/range.js
@@ -12,18 +12,51 @@
             this.max = min.max;
         } else {
             this.min = min;
-            this.max = max;
+            this.max = max !== undefined ? max : min;
         }
+        return this;
     };
     Range.prototype.asArray = function() {
         return [ this.min, this.max ];
     };
-    Range.prototype.diff = function() {
+    Range.prototype.length = function() {
         return this.max - this.min;
+    };
+    Range.prototype.empty = function() {
+        return this.max === this.min;
+    };
+    Range.prototype.inflate = function(min,max) {
+        var other = jstiny.asRange(min,max);
+        this.min -= other.min;
+        this.max += other.max;
+        return this;
+    };
+
+    Range.prototype.overlaps = function(min,max) {
+        var other = jstiny.asRange(min,max);
+        return this.min <= other.max && this.max >= other.min;
+    };
+    Range.prototype.inside = function(min,max) {
+        var other = jstiny.asRange(min,max);
+        return this.min >= other.min && this.max <= other.max;
+    };
+    Range.prototype.bind = function(min,max) {
+        var other = jstiny.asRange(min,max);
+        if (this.min < other.min) {
+            this.max = Math.min(other.max, other.min + this.length());
+            this.min = other.min;
+        } else if (this.max > other.max) {
+            this.min = Math.max(other.min, other.max - this.length());
+            this.max = other.max;
+        }
+        return this;
     };
 
     jstiny.range = function(min,max) {
         return new Range(min,max);
+    };
+    jstiny.asRange = function(min,max) {
+        return min instanceof Range ? min : jstiny.range(min,max);
     };
 
 
--- test/range.js
+++ test/range.js
@@ -1,68 +1,118 @@
 describe("range", function() {
     
-    it("range can be created from array", function() {
-        var range = jstiny.range([1,2]);
-        expect(range.min).toEqual(1);
-        expect(range.max).toEqual(2);
+    describe("range.ctor()", function() {
+        it("can be created from array", function() {
+            var range = jstiny.range([1,2]);
+            expect(range.min).toEqual(1);
+            expect(range.max).toEqual(2);
+        });
+        it("can be created from object", function() {
+            var range = jstiny.range({min:1, max:2});
+            expect(range.min).toEqual(1);
+            expect(range.max).toEqual(2);
+        });
+        it("can be created from parameters", function() {
+            var range = jstiny.range(1,2);
+            expect(range.min).toEqual(1);
+            expect(range.max).toEqual(2);
+        });
+        it("can be created from range", function() {
+            var range1 = jstiny.range(1,2),
+                range2 = jstiny.range(range1);
+            expect(range2.min).toEqual(1);
+            expect(range2.max).toEqual(2);
+        });
     });
 
-    it("range can be created from object", function() {
-        var range = jstiny.range({min:1, max:2});
-        expect(range.min).toEqual(1);
-        expect(range.max).toEqual(2);
+    describe("range.set()", function() {
+        it("accepts array", function() {
+            var range = jstiny.range(1,2);
+            expect(range.set([3,4])).toBe(range);
+            expect(range.min).toEqual(3);
+            expect(range.max).toEqual(4);
+        });
+        it("accepts object", function() {
+            var range = jstiny.range(1,2);
+            expect(range.set({min: 3, max: 4})).toBe(range);
+            expect(range.min).toEqual(3);
+            expect(range.max).toEqual(4);
+        });
+        it("accepts parameters", function() {
+            var range = jstiny.range(1,2);
+            expect(range.set(3,4)).toBe(range);
+            expect(range.min).toEqual(3);
+            expect(range.max).toEqual(4);
+        });
+        it("accepts range", function() {
+            var range1 = jstiny.range(1,2),
+                range2 = jstiny.range(3,4);
+            expect(range1.set(range2)).toBe(range1);
+            expect(range1.min).toEqual(3);
+            expect(range1.max).toEqual(4);
+        });
     });
 
-    it("range can be created from parameters", function() {
-        var range = jstiny.range(1,2);
-        expect(range.min).toEqual(1);
-        expect(range.max).toEqual(2);
-    });
-
-    it("range can be created from range", function() {
-        var range1 = jstiny.range(1,2),
-            range2 = jstiny.range(range1);
-        expect(range2.min).toEqual(1);
-        expect(range2.max).toEqual(2);
-    });
-
-    it("range.set accepts array", function() {
-        var range = jstiny.range(1,2);
-        range.set([3,4]);
-        expect(range.min).toEqual(3);
-        expect(range.max).toEqual(4);
-    });
-
-    it("range.set accepts object", function() {
-        var range = jstiny.range(1,2);
-        range.set({min: 3, max: 4});
-        expect(range.min).toEqual(3);
-        expect(range.max).toEqual(4);
-    });
-
-    it("range.set accepts parameters", function() {
-        var range = jstiny.range(1,2);
-        range.set(3,4);
-        expect(range.min).toEqual(3);
-        expect(range.max).toEqual(4);
-    });
-
-    it("range.set accepts range", function() {
-        var range1 = jstiny.range(1,2),
-            range2 = jstiny.range(3,4);
-        range1.set(range2);
-        expect(range1.min).toEqual(3);
-        expect(range1.max).toEqual(4);
-    });
-
-    it("range.diff returns distance", function() {
+    it("range.length() returns distance", function() {
         var range = jstiny.range(10, 100);
-        expect(range.diff()).toEqual(90);
+        expect(range.length()).toEqual(90);
     });
 
-    it("range.asArrays returns range as array", function() {
+    it("range.asArrays() returns range as array", function() {
         var range = jstiny.range(10, 100);
         expect(range.asArray()).toEqual([10,100]);
     });
 
+    it("range.empty() returns true when min === max, false otherwise", function() {
+        expect(jstiny.range(10, 90).empty()).toBe(false);
+        expect(jstiny.range(10, 10).empty()).toBe(true);
+    });
+
+    describe("range.inflate()", function() {
+        it("by one parameter", function() {
+            var range = jstiny.range(10, 90);
+            range.inflate(10);
+            expect(range.min).toEqual(0);
+            expect(range.max).toEqual(100);
+        });
+        it("by two parameters", function() {
+            var range = jstiny.range(10, 90);
+            range.inflate(10, 20);
+            expect(range.min).toEqual(0);
+            expect(range.max).toEqual(110);
+        });
+        it("by array", function() {
+            var range = jstiny.range(10, 90);
+            range.inflate([10, 20]);
+            expect(range.min).toEqual(0);
+            expect(range.max).toEqual(110);
+        });
+        it("by range", function() {
+            var range = jstiny.range(10, 90);
+            range.inflate(jstiny.range(10, 30));
+            expect(range.min).toEqual(0);
+            expect(range.max).toEqual(120);
+        });
+    });
+
+    describe("range.bind()", function() {
+        it("should move range to be inside of specified (left side)", function() {
+            var inner = jstiny.range(10,20), outer = jstiny.range(1000, 2000);
+            expect(inner.bind(outer)).toBe(inner);
+            expect(inner.min).toEqual(1000);
+            expect(inner.max).toEqual(1010);
+        });
+        it("should move range to be inside of specified (right side)", function() {
+            var inner = jstiny.range(5000,5100), outer = jstiny.range(1000, 2000);
+            expect(inner.bind(outer)).toBe(inner);
+            expect(inner.min).toEqual(1900);
+            expect(inner.max).toEqual(2000);
+        });
+        it("should make range equal to outer if inner is bigger than outer", function() {
+            var inner = jstiny.range(100,5000), outer = jstiny.range(200, 300);
+            expect(inner.bind(outer)).toBe(inner);
+            expect(inner.min).toEqual(200);
+            expect(inner.max).toEqual(300);
+        });
+    });
 
 });
